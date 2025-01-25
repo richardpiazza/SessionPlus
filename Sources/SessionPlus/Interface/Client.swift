fdbcf348238095085f64ed62763b816e2ee00852
Richard Piazza <richard@richardpiazza.com>
2025-01-25 15:17:55 -0600

    Removed Combine References; Simplified 'performRequest' Handling;

--- Sources/SessionPlus/Interface/Client.swift
+++ Sources/SessionPlus/Interface/Client.swift
@@ -1,66 +1,23 @@
 import Foundation
-#if canImport(Combine)
-import Combine
-#endif
 
 public protocol Client {
-    #if swift(>=5.5.2)
     /// Perform a network `Request`.
     ///
     /// - parameters:
     ///   - request: The details of the request to perform.
     /// - returns: The `Response` to the `Request`.
-    func performRequest(_ request: Request) async throws -> Response
-    #endif
-    
-    #if canImport(Combine)
-    /// Perform a network `Request`.
-    ///
-    /// - parameters:
-    ///   - request: The details of the request to perform.
-    /// - returns: Publisher that emits the `Response` to the `Request`.
-    func performRequest(_ request: Request) -> AnyPublisher<Response, Error>
-    #endif
-    
-    /// Perform a network `Request`.
-    ///
-    /// - parameters:
-    ///   - request: The details of the request to perform.
-    ///   - completion: Function called with the result of the request.
-    func performRequest(_ request: Request, completion: @escaping (Result<Response, Error>) -> Void)
+    func performRequest(_ request: any Request) async throws -> any Response
 }
 
 public extension Client {
-    #if swift(>=5.5.2)
-    /// Default implementation that uses the `withCheckedThrowingContinuation` api to call the `performRequest(_:)` method.
-    func performRequest(_ request: Request) async throws -> Response {
-        try await withCheckedThrowingContinuation({ continuation in
-            performRequest(request) { result in
-                switch result {
-                case .failure(let error):
-                    continuation.resume(throwing: error)
-                case .success(let response):
-                    continuation.resume(returning: response)
-                }
-            }
-        })
+    /// Performs a network `Request` and decodes the response to a known type.
+    ///
+    /// - parameters:
+    ///   - request: The details of the request to perform.
+    ///   - decoder: The `JSONDecoder` that should be used to deserialize the result data.
+    /// - returns: The decoded `Response` value.
+    func performRequest<Value>(_ request: any Request, using decoder: JSONDecoder = JSONDecoder()) async throws -> Value where Value: Decodable {
+        let response = try await performRequest(request)
+        return try decoder.decode(Value.self, from: response.data)
     }
-    #endif
-    
-    #if canImport(Combine)
-    /// Default implementation that wraps `performRequest(_:)` in a `Future`.
-    func performRequest(_ request: Request) -> AnyPublisher<Response, Error> {
-        Future { promise in
-            performRequest(request) { result in
-                switch result {
-                case .failure(let error):
-                    promise(.failure(error))
-                case .success(let response):
-                    promise(.success(response))
-                }
-            }
-        }
-        .eraseToAnyPublisher()
-    }
-    #endif
 }
